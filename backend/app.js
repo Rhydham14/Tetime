02014ac16070ead721578570c1a0c21885ab85be
rhydham14 <rhydham.bhalodia122@gmail.com>
2024-05-14 10:32:36 +0530

    cors link added

--- backend/app.js
+++ backend/app.js
@@ -12,19 +12,16 @@ require("dotenv").config();
 //   "https://tetime.vercel.app",
 //   "http://localhost:3000"  
 // ];
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  next(); // Call next middleware
-});
 
 app.use(express.json());
 app.use(cors({
   origin: [
     "https://tetime-q22048yek-rhydham14s-projects.vercel.app",
+    "https://tetime-q22048yek-rhydham14s-projects.vercel.app/",
     "https://tetime.vercel.app",
-    "http://localhost:3000"
+    "https://tetime.vercel.app/",
+    "http://localhost:3000",
+    "http://localhost:3000/"
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization"
