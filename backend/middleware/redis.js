0777f53c95d0fedb3ea18e65cce2168ba5f77e81
rhydham14 <rhydham.bhalodia122@gmail.com>
2024-05-20 17:28:07 +0530

    redis added

--- backend/middleware/redis.js
+++ backend/middleware/redis.js
@@ -1,28 +1,8 @@
-// const redis = require('redis');
-
-// // Create a Redis client
-// const client = redis.createClient();
-  
-// // Handle Redis connection errors
-// client.on('error', (err) => {
-//   console.error('Error connecting to Redis:', err);
-// });
-
-// // Connect to Redis
-// client.connect().then(() => {
-//   console.log('Connected to Redis server');
-// });
-
-// module.exports = client;
-
-
-
 const redis = require('redis');
 
-// Use environment variable for Redis URL
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
-const client = redis.createClient({ url: redisUrl });
-
+// Create a Redis client
+const client = redis.createClient();
+  
 // Handle Redis connection errors
 client.on('error', (err) => {
   console.error('Error connecting to Redis:', err);
@@ -34,3 +14,23 @@ client.connect().then(() => {
 });
 
 module.exports = client;
+
+
+
+// const redis = require('redis');
+
+// // Use environment variable for Redis URL
+// const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+// const client = redis.createClient({ url: redisUrl });
+
+// // Handle Redis connection errors
+// client.on('error', (err) => {
+//   console.error('Error connecting to Redis:', err);
+// });
+
+// // Connect to Redis
+// client.connect().then(() => {
+//   console.log('Connected to Redis server');
+// });
+
+// module.exports = client;
